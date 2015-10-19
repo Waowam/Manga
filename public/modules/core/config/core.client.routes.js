8190dce6a08757a9d04eff24f5131fa5d503d048
Jérémy Pinto <jeremy.pinto82@gmail.com>
2015-10-19 22:17:17 +0200

    Initial commit for meanjs
    Integrate Magnetic template with meanjs stack + assets + begin menu and
    tooltip directive.

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,14 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		})
+		.state('contacts',{
+			url: '/contacts',
+			templateUrl: 'modules/core/views/contact.client.view.html'
+		})
+		.state('details',{
+			url: '/details',
+			templateUrl: 'modules/core/views/details.client.view.html'
 		});
 	}
 ]);
---
+++ public/modules/core/directives/tooltip/tooltip.client.directive.js
@@ -0,0 +1,44 @@
+'use strict';
+angular.module('core.tooltip-directive', [])
+
+.directive('tooltip', function() {
+  return {
+    restrict: 'A',
+    scope: {},
+    link: function (scope, element, attrs) {
+      //TODO Fix this whenever we want if we want tooltips
+    }
+  };
+}]);
+
+// var attr_title = $(this).attr("data-title");
+//
+// if( attr_title == undefined || attr_title == "") return false;
+//
+// $(this).after('<span class="tooltip"></span>');
+//
+// var tooltip = $(".tooltip");
+// tooltip.append($(this).data('title'));
+//
+//
+// var tipwidth = tooltip.outerWidth();
+// var a_width = $(this).width();
+// var a_hegiht = $(this).height() + 3 + 4;
+//
+// //if the tooltip width is smaller than the a/link/parent width
+// if(tipwidth < a_width){
+//     tipwidth = a_width;
+//     $('.tooltip').outerWidth(tipwidth);
+// }
+//
+// var tipwidth = '-' + (tipwidth - a_width)/2;
+// $('.tooltip').css({
+//     'left' : tipwidth + 'px',
+//     'bottom' : a_hegiht + 'px'
+// }).stop().animate({
+//     opacity : 1
+// }, 200);
+// $("a").mouseout(function(){
+//     var tooltip = $(".tooltip");
+//     tooltip.remove();
+// });
